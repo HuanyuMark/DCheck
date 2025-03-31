78fe89a9746fb2017139c9b8386ca3b4e41d07d1
Sunsy <huanyumail101@qq.com>
2025-04-01 01:50:27 +0800

    impl JdbcAllowListRuleSet after impl  JdbcAgent

--- DCheck-Impl-WhiteListManager-JDBC/src/main/java/org/example/dcheck/impl/wlm/jdbc/core/JdbcAllowListRuleSet.java
+++ DCheck-Impl-WhiteListManager-JDBC/src/main/java/org/example/dcheck/impl/wlm/jdbc/core/JdbcAllowListRuleSet.java
@@ -1,11 +1,11 @@
-package org.example.dcheck.impl.wlm.jdbc.core;
-
-import org.example.dcheck.api.AllowListRuleSet;
-
-/**
- * Date 2025/03/31
- *
- * @author 三石而立Sunsy
- */
-public class JdbcAllowListRuleSet implements AllowListRuleSet {
-}
+//package org.example.dcheck.impl.wlm.jdbc.core;
+//
+//import org.example.dcheck.api.AllowListRuleSet;
+//
+///**
+// * Date 2025/03/31
+// *
+// * @author 三石而立Sunsy
+// */
+//public class JdbcAllowListRuleSet implements AllowListRuleSet {
+//}
